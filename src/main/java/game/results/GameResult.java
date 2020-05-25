f4b7fb013c775064b460ad730fe455b9e72c7031
marcszeg <marcszeg27@gmail.com>
2020-05-25 11:50:05 +0200

    Added files for the 'Highscores' database.

---
+++ src/main/java/game/results/GameResult.java
@@ -0,0 +1,55 @@
+package game.results;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+/**
+ * Class representing the result of a game played by a specific player.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class GameResult {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * The name of the winner player.
+     */
+    @Column(nullable = false)
+    private String winPlayer;
+
+    /**
+     * The name of the opponent player.
+     */
+    @Column(nullable = false)
+    private String opPlayer;
+
+    /**
+     * The duration of the game.
+     */
+    @Column(nullable = false)
+    private Duration duration;
+
+    /**
+     * The timestamp when the result was saved.
+     */
+    @Column(nullable = false)
+    private ZonedDateTime created;
+
+    @PrePersist
+    protected void onPersist() {
+        created = ZonedDateTime.now();
+    }
+
+}
