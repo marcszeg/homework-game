749d9c8892096db161fc97231fccb4410d8994c0
marcszeg <marcszeg27@gmail.com>
2020-05-26 19:08:22 +0200

    Added SlotTest.

--- src/test/java/homeworkGame/state/HomeworkGameStateTest.java
+++ src/test/java/homeworkGame/state/HomeworkGameStateTest.java
@@ -8,18 +8,57 @@ class HomeworkGameStateTest {
 
     @Test
     void isBlueWon() {
+        //assertFalse(new HomeworkGameState().isBlueWon());
+        /*assertTrue(new HomeworkGameState(new int[][] {
+                {0, 0, 0, 0, 1},
+                {2, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0}}).isBlueWon());
+        assertFalse(new HomeworkGameState(new int[][] {
+                {0, 0, 0, 2, 1},
+                {2, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 2, 0, 0, 0},
+                {0, 1, 0, 0, 0}}).isBlueWon());*/
     }
 
     @Test
     void isRedWon() {
+        //assertFalse(new HomeworkGameState().isRedWon());
+        /*assertTrue(new HomeworkGameState(new int[][] {
+                {1, 0, 1, 0, 1},
+                {2, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}}).isRedWon());
+        assertFalse(new HomeworkGameState(new int[][] {
+                {0, 0, 0, 2, 1},
+                {2, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 2, 0, 0, 0},
+                {0, 1, 0, 0, 0}}).isRedWon());*/
     }
 
     @Test
     void canMove() {
+        /*assertTrue(new HomeworkGameState(new int[][] {
+                {0, 0, 0, 2, 1},
+                {2, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 2, 0, 0, 0},
+                {0, 1, 0, 0, 0}}).canMove(0, 4, 0, 3));*/
+        assertFalse(new HomeworkGameState(new int[][] {
+                {0, 0, 0, 2, 1},
+                {2, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 2, 0, 0, 0},
+                {0, 1, 0, 0, 0}}).canMove(1, 3, 2, 3));
     }
 
     @Test
     void moveDisc() {
+
     }
 
     @Test
