73f48e9890f4955e59b003669aa5ff2a97f0f213
marcszeg <marcszeg27@gmail.com>
2020-05-27 00:23:01 +0200

    Main.java and HomeworkGameApplication.java finalized.

---
+++ src/main/java/homeworkGame/javafx/Main.java
@@ -0,0 +1,10 @@
+package homeworkGame.javafx;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(HomeworkGameApplication.class, args);
+    }
+
+}
